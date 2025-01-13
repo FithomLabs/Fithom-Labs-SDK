dad1efe30f82d4b074979f18f13db1a2b0531a0b
FithomLabs <xxxwalken05@gmail.com>
2025-01-13 20:59:20 +0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const wallet = require('./wallet');
+const transactions = require('./transactions');
+const analytics = require('./analytics');
+
+module.exports = {
+  wallet,
+  transactions,
+  analytics,
+};
